b4de0099bf7b2f4b75f6d903975ecf8f144c3cfe
Ármin Scipiades <scipiades.armin@gmail.com>
2011-10-01 15:14:23 +0200

    Added title helper for base title, changed separator into ::

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+# title helper to ensure we have a valid & nicely looking title on all our pages
+def title
+	base_title="Railstry"
+	if @title.nil?
+		base title
+	else
+		"#{base_title}::#{@title}"
+	end
+end
+
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,7 @@ describe PagesController do
 	render_views
 
 	before(:each) do
-		@base_title="Railstry | "
+		@base_title="Railstry::"
 	end
 
 	describe "GET 'home'" do
