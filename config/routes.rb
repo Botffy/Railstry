85117afd845cdaa3b3a1d81c49bc0c1754d08e44
Ármin Scipiades <scipiades.armin@gmail.com>
2011-10-01 18:10:52 +0200

    I'm lame. Did navigation, with appropriate routing, added basic styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Firstapp::Application.routes.draw do
-	#static pages
-  	get "pages/home"
-  	get "pages/contact"
-  	get "pages/about"
-
-  resources :microposts
-  resources :users
-
 	#http://guides.rubyonrails.org/routing.html	
 
-  root :to => 'pages#home'
+	resources :microposts
+	resources :users
+
+	match '/contact', :to => 'pages#contact'
+	match '/about', :to => 'pages#about'
+
+  	root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 end
