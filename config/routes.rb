10aa1b5d7c61304cb7e19a10505e6f0f5f4ac9a9
Ármin Scipiades <scipiades.armin@gmail.com>
2011-11-29 00:59:54 +0100

    Added help page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Firstapp::Application.routes.draw do
 
 	match '/contact', :to => 'pages#contact'
 	match '/about', :to => 'pages#about'
+	match '/help', :to => 'pages#help'
 
   	root :to => 'pages#home'
 
