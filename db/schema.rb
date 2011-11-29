3f08af1a570298cc9443403a7c9c49493ef1c375
Ármin Scipiades <scipiades.armin@gmail.com>
2011-11-29 02:47:15 +0100

    Dumped new schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,18 +13,4 @@
 
 ActiveRecord::Schema.define(:version => 20110927014404) do
 
-  create_table "microposts", :force => true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
