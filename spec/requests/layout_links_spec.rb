cf474b5af5bf739d5bfad4bbd95d35f13f03207f
Ármin Scipiades <scipiades.armin@gmail.com>
2011-11-29 02:55:30 +0100

    Added test: root page should have the correct links leading to the correct pages

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -37,4 +37,22 @@ describe "LayoutLinks" do
 		response.should have_selector("title", :content=>@base_title+"Signup")
 	end
 
+	it "should have the right links leading to the right pages" do
+		visit root_path
+			click_link "About"
+			response.should have_selector('title', :content => "About")
+
+			click_link "Help"
+			response.should have_selector('title', :content => "Help")
+
+			click_link "Contact"
+			response.should have_selector('title', :content => "Contact")
+
+			click_link "Home"
+			response.should have_selector('title', :content => "Home")
+
+			click_link "Sign up now!"
+			response.should have_selector('title', :content => "Signup")
+	end
+
 end
