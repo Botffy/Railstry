81ea08d198d8afcad905cf235ced67e05ff836df
Ármin Scipiades <scipiades.armin@gmail.com>
2011-12-03 13:01:14 +0100

    Made gravatar use 'wavatar' defaults

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
 	def gravatar_for(user, options={ :size=>50 } )
+		options[:default]=:wavatar if options[:default].nil?
+		options[:rating]='pg' if options[:rating].nil?
 		gravatar_image_tag(user.email.downcase, :alt=>h(user.name), :class=>'gravatar', :gravatar=>options )
-
 	end
 end
